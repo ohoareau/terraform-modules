42016460b9eb6112a8143f14f7bcd1bde821edfa
Olivier Hoareau <oha@greenberets.io>
2019-10-14 13:37:33 +0200

    add user_pool capability for appsync api

--- modules/appsync-api/main.tf
+++ modules/appsync-api/main.tf
@@ -2,12 +2,21 @@ data "local_file" "schema" {
   filename = var.schema_file
 }
 resource "aws_appsync_graphql_api" "api" {
-  authentication_type = "${var.auth_type}"
+  authentication_type = var.auth_type
   name                = var.name
   schema              = data.local_file.schema.content
   log_config {
     cloudwatch_logs_role_arn = aws_iam_role.logs.arn
     field_log_level          = "ERROR"
+  }
+  dynamic "user_pool_config" {
+    iterator = v
+    for_each = ("" == var.user_pool_id) ? {} : {user_pool: true}
+    content {
+      aws_region     = var.user_pool_region
+      default_action = "DENY"
+      user_pool_id   = var.user_pool_id
+    }
   }
 }
 
--- modules/appsync-api/variables.tf
+++ modules/appsync-api/variables.tf
@@ -7,3 +7,9 @@ variable "schema_file" {
 variable "auth_type" {
   default = "AWS_IAM"
 }
+variable "user_pool_region" {
+  default = ""
+}
+variable "user_pool_id" {
+  default = ""
+}
