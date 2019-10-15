756c57eb866f0ede04461ab83553ee43d8bca0a3
Olivier Hoareau <oha@greenberets.io>
2019-10-15 22:48:58 +0200

    fix cloudfront/s3 bug

--- modules/website/main.tf
+++ modules/website/main.tf
@@ -26,6 +26,13 @@ resource "aws_cloudfront_distribution" "website" {
   origin {
     domain_name         = aws_s3_bucket.website.website_endpoint
     origin_id           = "website-${var.name}-s3"
+    custom_origin_config {
+      // These are all the defaults.
+      http_port              = "80"
+      https_port             = "443"
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
     s3_origin_config {
       origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
     }
