f70f9adc0e366516eb939514ecd6289f7f71eea4
Olivier Hoareau <oha@greenberets.io>
2019-10-16 00:15:42 +0200

    fix

--- modules/website/main.tf
+++ modules/website/main.tf
@@ -193,10 +193,18 @@ resource "aws_route53_record" "cert_validation" {
   records = [aws_acm_certificate.cert.domain_validation_options.0.resource_record_value]
   ttl     = 60
 }
+resource "aws_route53_record" "cert_validation_alt" {
+  count   = var.apex_redirect ? 1 : 0
+  name    = aws_acm_certificate.cert.domain_validation_options.1.resource_record_name
+  type    = aws_acm_certificate.cert.domain_validation_options.1.resource_record_type
+  zone_id = var.zone
+  records = [aws_acm_certificate.cert.domain_validation_options.1.resource_record_value]
+  ttl     = 60
+}
 resource "aws_acm_certificate_validation" "cert" {
   provider                = "aws.acm"
   certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
+  validation_record_fqdns = var.apex_redirect ? [aws_route53_record.cert_validation.fqdn] : [aws_route53_record.cert_validation.fqdn, aws_route53_record.cert_validation_alt[0].fqdn]
 }
 
 data "aws_iam_policy_document" "s3_website_policy" {
