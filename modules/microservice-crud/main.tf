eb282568934e9849246816d009b15aa63c51df25
Olivier Hoareau <oha@greenberets.io>
2019-11-17 19:15:15 +0100

    big enhancements to use for_each / meta microservice module

--- modules/microservice-crud/main.tf
+++ modules/microservice-crud/main.tf
@@ -4,20 +4,20 @@ locals {
   upper_name_plural = title(("" != var.name_plural) ? var.name_plural : "${var.name}s")
   prefix            = "${var.env}-${var.name}"
   operations = {
-    events = merge({api: false, enabled: true, policy_statements = []}, lookup(var.operations, "events", {}))
-    list   = merge({api: true, enabled: true, policy_statements = []}, lookup(var.operations, "list", {}))
-    get    = merge({api: true, enabled: true, policy_statements = []}, lookup(var.operations, "get", {}))
-    delete = merge({api: true, enabled: true, policy_statements = []}, lookup(var.operations, "delete", {}))
-    create = merge({api: true, enabled: true, policy_statements = []}, lookup(var.operations, "create", {}))
-    update = merge({api: true, enabled: true, policy_statements = []}, lookup(var.operations, "update", {}))
+    events = merge({api = false, enabled = true, policy_statements = []}, lookup(var.operations, "events", {}))
+    list   = merge({api = true, enabled = true, policy_statements = []}, lookup(var.operations, "list", {}))
+    get    = merge({api = true, enabled = true, policy_statements = []}, lookup(var.operations, "get", {}))
+    delete = merge({api = true, enabled = true, policy_statements = []}, lookup(var.operations, "delete", {}))
+    create = merge({api = true, enabled = true, policy_statements = []}, lookup(var.operations, "create", {}))
+    update = merge({api = true, enabled = true, policy_statements = []}, lookup(var.operations, "update", {}))
   }
   enabled_operations = {
-    events = false != lookup(lookup(var.operations, "events", {enabled: true}), "enabled", true)
-    list   = false != lookup(lookup(var.operations, "list", {enabled: true}), "enabled", true)
-    get    = false != lookup(lookup(var.operations, "get", {enabled: true}), "enabled", true)
-    delete = false != lookup(lookup(var.operations, "delete", {enabled: true}), "enabled", true)
-    create = false != lookup(lookup(var.operations, "create", {enabled: true}), "enabled", true)
-    update = false != lookup(lookup(var.operations, "update", {enabled: true}), "enabled", true)
+    events = false != lookup(lookup(var.operations, "events", {enabled = true}), "enabled", true)
+    list   = false != lookup(lookup(var.operations, "list", {enabled = true}), "enabled", true)
+    get    = false != lookup(lookup(var.operations, "get", {enabled = true}), "enabled", true)
+    delete = false != lookup(lookup(var.operations, "delete", {enabled = true}), "enabled", true)
+    create = false != lookup(lookup(var.operations, "create", {enabled = true}), "enabled", true)
+    update = false != lookup(lookup(var.operations, "update", {enabled = true}), "enabled", true)
   }
 }
 
