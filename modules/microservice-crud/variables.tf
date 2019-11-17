c73f36ae54723a4d80d58d6639e44e203eb641a4
Olivier Hoareau <oha@greenberets.io>
2019-11-17 18:14:44 +0100

    big enhancements to use for_each / meta microservice module

--- modules/microservice-crud/variables.tf
+++ modules/microservice-crud/variables.tf
@@ -8,13 +8,18 @@ variable "name" {
   type = string
 }
 variable "name_plural" {
+  type    = string
   default = ""
 }
 variable "operations" {
+  type = "map"
+  default = {}
 }
 variable "api" {
+  type    = string
   default = ""
 }
 variable "api_name" {
+  type    = string
   default = ""
 }
