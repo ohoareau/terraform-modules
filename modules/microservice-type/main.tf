384fe6d1e3fdb8afce4b666b6abcc78fb9e4436b
Olivier Hoareau <oha@greenberets.io>
2019-11-26 15:48:16 +0100

    fix

--- modules/microservice-type/main.tf
+++ modules/microservice-type/main.tf
@@ -5,8 +5,8 @@ locals {
 }
 
 locals {
-  full_name              = (var.parent != null) ? "${var.parent.full_name}${var.name}" : var.name
-  full_name_plural       = (var.parent != null) ? "${var.parent.full_upper_name}${local.name_plural}" : local.name_plural
+  full_name              = (var.parent != null) ? "${var.parent.full_name}-${var.name}" : var.name
+  full_name_plural       = (var.parent != null) ? "${var.parent.full_upper_name}${local.upper_name_plural}" : local.name_plural
   full_upper_name        = (var.parent != null) ? "${var.parent.full_upper_name}${local.upper_name}" : local.upper_name
   upper_name_plural      = title(local.name_plural)
   full_upper_name_plural = title(local.full_name_plural)
