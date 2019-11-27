5082dd932b381cc4d0ce17462b444c4a3a3b2cb3
Olivier Hoareau <oha@greenberets.io>
2019-11-27 17:35:10 +0100

    fix

--- modules/ses/outputs.tf
+++ modules/ses/outputs.tf
@@ -1,3 +1,15 @@
+output "identity" {
+  value = aws_ses_domain_identity.identity.id
+}
+output "identity_arn" {
+  value = aws_ses_domain_identity.identity.arn
+}
+output "verification_token" {
+  value = aws_ses_domain_identity.identity.verification_token
+}
+output "domain" {
+  value = aws_ses_domain_identity.identity.domain
+}
 output "email_identities" {
   value = {for k,v in aws_ses_email_identity.identities: k => {arn: v.arn, email: "${var.emails[k]}@${var.domain}"}}
 }
