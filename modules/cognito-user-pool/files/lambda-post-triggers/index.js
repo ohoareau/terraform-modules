88479310242f0f83988cef58849317604979a6c1
Olivier Hoareau <oha@greenberets.io>
2019-11-29 12:34:27 +0100

    fix

--- modules/cognito-user-pool/files/lambda-post-triggers/index.js
+++ modules/cognito-user-pool/files/lambda-post-triggers/index.js
@@ -1,5 +1,28 @@
+const AWS = require('aws-sdk');
+const sns = new AWS.SNS();
+
+const dispatch = async (type, operation, payload = {}, attributes = {}) =>
+    sns.publish({
+        Message: JSON.stringify(await payload),
+        MessageAttributes: Object.entries({...attributes, type, operation, fullType: `${type}_${operation}`}).reduce((acc, [k, v]) => {
+            acc[k] = {DataType: 'String', StringValue: v};
+            return acc;
+        }, {}),
+        TopicArn: process.env.OUTGOING_TOPIC_ARN,
+    }).promise()
+;
+
 module.exports = {
     handler: async (event) => {
-        console.log(event);
+        if (!event || !event.triggerSource) return; // ignore
+        const { triggerSource, request, response, ...source } = event;
+        switch (triggerSource) {
+            case 'PostAuthentication_Authentication':
+                const { userAttributes: user, ...extra } = request;
+                await dispatch('user', 'login', {extra, source, ...user});
+                break;
+            default: // ignore
+                break;
+        }
     },
 };
