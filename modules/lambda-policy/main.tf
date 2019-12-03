15eb3281c7fdea3e51eb2810120a0fc39216b315
Olivier Hoareau <oha@greenberets.io>
2019-12-03 01:38:12 +0100

    fix

--- modules/lambda-policy/main.tf
+++ modules/lambda-policy/main.tf
@@ -18,8 +18,9 @@ data "aws_iam_policy_document" "lambda" {
 }
 
 resource "aws_iam_role_policy" "lambda" {
-  count  = var.enabled ? 1 : 0
-  name   = "lambda-${var.name}-policy-${var.policy_name}"
-  policy = data.aws_iam_policy_document.lambda.json
-  role   = var.role_name
+  count       = var.enabled ? 1 : 0
+  name        = (128 < length("lambda-${var.name}-policy-${var.policy_name}")) ? null : "lambda-${var.name}-policy-${var.policy_name}"
+  name_prefix = (128 >= length("lambda-${var.name}-policy-${var.policy_name}")) ? null : "lambda-policy-"
+  policy      = data.aws_iam_policy_document.lambda.json
+  role        = var.role_name
 }
