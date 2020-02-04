3f381c90dd4069172ea2b0b8ab7d75735458b55d
Olivier Hoareau <oha@greenberets.io>
2020-02-04 10:06:24 +0100

    fix

--- modules/lambda/outputs.tf
+++ modules/lambda/outputs.tf
@@ -4,6 +4,9 @@ output "name" {
 output "arn" {
   value = (var.enabled && 0 < length(aws_lambda_function.lambda)) ? aws_lambda_function.lambda[0].arn : null
 }
+output "invoke_arn" {
+  value = (var.enabled && 0 < length(aws_lambda_function.lambda)) ? aws_lambda_function.lambda[0].invoke_arn : null
+}
 output "role_arn" {
   value = (var.enabled && 0 < length(aws_lambda_function.lambda)) ? aws_iam_role.lambda[0].arn : null
 }
