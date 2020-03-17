f0e6cfc75d7abddba7e25f305550604ca58815d9
Olivier Hoareau <oha@greenberets.io>
2020-03-17 13:04:29 +0100

    fix

--- modules/dynamodb-table/outputs.tf
+++ modules/dynamodb-table/outputs.tf
@@ -1,6 +1,6 @@
 output "arn" {
-  value = var.enabled ? aws_dynamodb_table.table[0].arn : null
+  value = (var.enabled && (length(aws_dynamodb_table.table) > 0)) ? aws_dynamodb_table.table[0].arn : null
 }
 output "name" {
-  value = var.enabled ? aws_dynamodb_table.table[0].name : null
+  value = (var.enabled && (length(aws_dynamodb_table.table) > 0)) ? aws_dynamodb_table.table[0].name : null
 }
