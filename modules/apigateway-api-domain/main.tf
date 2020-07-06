eabe7e9742f452746ea5164b5d39928861fcf97e
Olivier Hoareau <oha@greenberets.io>
2020-07-06 13:46:03 +0200

    apigateway-api-domain: creation (for apigw v1)

---
+++ modules/apigateway-api-domain/main.tf
@@ -0,0 +1,56 @@
+provider "aws" {
+}
+provider "aws" {
+  alias = "acm"
+}
+
+resource "aws_api_gateway_domain_name" "api" {
+  depends_on               = [aws_acm_certificate_validation.cert]
+  domain_name              = var.dns
+  regional_certificate_arn = aws_acm_certificate.cert.arn
+  security_policy          = "TLS_1_2"
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+resource "aws_api_gateway_base_path_mapping" "api" {
+  api_id      = var.api
+  stage_name  = var.stage
+  domain_name = aws_api_gateway_domain_name.api.domain_name
+}
+
+resource "aws_route53_record" "api" {
+  zone_id = var.zone
+  name    = aws_api_gateway_domain_name.api.domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_api_gateway_domain_name.api.regional_domain_name
+    zone_id                = aws_api_gateway_domain_name.api.regional_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_acm_certificate" "cert" {
+  domain_name       = var.dns
+  validation_method = "DNS"
+  provider          = aws.acm
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "cert_validation" {
+  name    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_name
+  type    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_type
+  zone_id = var.zone
+  records = [aws_acm_certificate.cert.domain_validation_options.0.resource_record_value]
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "cert" {
+  provider                = aws.acm
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
+}
