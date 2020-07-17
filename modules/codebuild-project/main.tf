20f1512710e19d4e20babc7aa3f305a5f39f3037
Olivier Hoareau <oha@greenberets.io>
2020-07-17 16:14:21 +0200

    fix

--- modules/codebuild-project/main.tf
+++ modules/codebuild-project/main.tf
@@ -9,6 +9,20 @@ locals {
         ]
         resources = ["*"]
         effect    = "Allow"
+      },
+      {
+        actions = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:GetRepositoryPolicy",
+          "ecr:DescribeRepositories",
+          "ecr:ListImages",
+          "ecr:DescribeImages",
+          "ecr:BatchGetImage",
+        ],
+        resources = ["*"],
+        effect = "Allow"
       }
     ],
     var.policy_statements
