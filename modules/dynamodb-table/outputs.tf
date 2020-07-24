945ef5a5805ed361fdceff7b618e5922f105ed8c
Olivier Hoareau <oha@greenberets.io>
2020-07-24 18:42:46 +0200

    dynamodb-table: fix

--- modules/dynamodb-table/outputs.tf
+++ modules/dynamodb-table/outputs.tf
@@ -5,8 +5,8 @@ output "name" {
   value = (var.enabled && (length(aws_dynamodb_table.table) > 0)) ? aws_dynamodb_table.table[0].name : null
 }
 output "stream_arn" {
-  value = (var.enabled && (length(aws_dynamodb_table.table) > 0) && aws_dynamodb_table.table[0].stream_enabled) ? aws_dynamodb_table.table[0].stream_arn : null
+  value = (var.enabled && (length(aws_dynamodb_table.table) > 0) && (null != aws_dynamodb_table.table[0].stream_enabled) && (true == aws_dynamodb_table.table[0].stream_enabled)) ? aws_dynamodb_table.table[0].stream_arn : null
 }
 output "stream_label" {
-  value = (var.enabled && (length(aws_dynamodb_table.table) > 0) && aws_dynamodb_table.table[0].stream_enabled) ? aws_dynamodb_table.table[0].stream_label : null
+  value = (var.enabled && (length(aws_dynamodb_table.table) > 0) && (null != aws_dynamodb_table.table[0].stream_enabled) && (true == aws_dynamodb_table.table[0].stream_enabled)) ? aws_dynamodb_table.table[0].stream_label : null
 }
