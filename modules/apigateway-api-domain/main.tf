7ab0f910d942cf57279f59b54e0f85006b9f7264
Olivier Hoareau <oha@greenberets.io>
2020-10-02 22:18:47 +0200

    fix: compatibility with aws provider 3.9

--- modules/apigateway-api-domain/main.tf
+++ modules/apigateway-api-domain/main.tf
@@ -42,10 +42,10 @@ resource "aws_acm_certificate" "cert" {
 }
 
 resource "aws_route53_record" "cert_validation" {
-  name    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_name
-  type    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_type
+  name    = element(tolist(aws_acm_certificate.cert.domain_validation_options), 0).resource_record_name
+  type    = element(tolist(aws_acm_certificate.cert.domain_validation_options), 0).resource_record_type
   zone_id = var.zone
-  records = [aws_acm_certificate.cert.domain_validation_options.0.resource_record_value]
+  records = [element(tolist(aws_acm_certificate.cert.domain_validation_options), 0).resource_record_value]
   ttl     = 60
 }
 
--- modules/apigateway2-api-domain/main.tf
+++ modules/apigateway2-api-domain/main.tf
@@ -26,8 +26,8 @@ resource "aws_route53_record" "api" {
   type    = "A"
 
   alias {
-    name                   = aws_apigatewayv2_domain_name.api.domain_name_configuration.0.target_domain_name
-    zone_id                = aws_apigatewayv2_domain_name.api.domain_name_configuration.0.hosted_zone_id
+    name                   = element(tolist(aws_apigatewayv2_domain_name.api.domain_name_configuration), 0).target_domain_name
+    zone_id                = element(tolist(aws_apigatewayv2_domain_name.api.domain_name_configuration), 0).hosted_zone_id
     evaluate_target_health = false
   }
 }
@@ -42,10 +42,10 @@ resource "aws_acm_certificate" "cert" {
 }
 
 resource "aws_route53_record" "cert_validation" {
-  name    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_name
-  type    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_type
+  name    = element(tolist(aws_acm_certificate.cert.domain_validation_options), 0).resource_record_name
+  type    = element(tolist(aws_acm_certificate.cert.domain_validation_options), 0).resource_record_type
   zone_id = var.zone
-  records = [aws_acm_certificate.cert.domain_validation_options.0.resource_record_value]
+  records = [element(tolist(aws_acm_certificate.cert.domain_validation_options), 0).resource_record_value]
   ttl     = 60
 }
 
