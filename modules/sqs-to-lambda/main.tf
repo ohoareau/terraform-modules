6ddcb30bba32b89e4251d77e74338acc583f2b1e
Olivier Hoareau <oha@greenberets.io>
2021-01-26 09:53:02 +0100

    feat(sqs-to-lambda): add several timeout/timer variables

--- modules/sqs-to-lambda/main.tf
+++ modules/sqs-to-lambda/main.tf
@@ -1,5 +1,8 @@
 resource "aws_sqs_queue" "queue" {
-  name = var.name
+  name                       = var.name
+  visibility_timeout_seconds = var.visibility_timeout_seconds
+  message_retention_seconds  = var.message_retention_seconds
+  max_message_size           = var.max_message_size
 }
 
 data "aws_sqs_queue" "queue" {
--- modules/sqs-to-lambda/variables.tf
+++ modules/sqs-to-lambda/variables.tf
@@ -7,3 +7,27 @@ variable "lambda_arn" {
 variable "lambda_role_name" {
   type    = string
 }
+variable "visibility_timeout_seconds" {
+  type    = number
+  default = null
+}
+variable "message_retention_seconds" {
+  type    = number
+  default = null
+}
+variable "max_message_size" {
+  type    = number
+  default = null
+}
+variable "delay_seconds" {
+  type    = number
+  default = null
+}
+variable "receive_wait_time_seconds" {
+  type    = number
+  default = null
+}
+variable "fifo_queue" {
+  type    = bool
+  default = null
+}
