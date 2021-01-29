cf81a7ccf12dedf183cef7ae61459e80dd828207
Olivier Hoareau <oha@greenberets.io>
2021-01-29 15:32:21 +0100

    feat(layers/network): smtps option

--- layers/com-basic/main.tf
+++ layers/com-basic/main.tf
@@ -5,7 +5,7 @@ locals {
   identities = {
     main   = module.ses-regional-identity
   }
-  smtp_user_infos = (null != var.smtp_user_name) ? {name = module.ses-smtp-user.name, arn = module.ses-smtp-user.arn, password = module.ses-smtp-user.password, login = module.ses-smtp-user.login} : null
+  smtp_user_infos = (null != var.smtp_user_name) ? {name = module.ses-smtp-user[0].name, arn = module.ses-smtp-user[0].arn, password = module.ses-smtp-user[0].password, login = module.ses-smtp-user[0].login} : null
 }
 
 module "ses-global" {
