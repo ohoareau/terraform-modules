bfd08830791945e32e51cf6502540c68ad9915fb
Olivier Hoareau <oha@greenberets.io>
2021-01-29 12:40:56 +0100

    fix(layers/com-basic): fix

--- layers/com-basic/main.tf
+++ layers/com-basic/main.tf
@@ -5,6 +5,7 @@ locals {
   identities = {
     main   = module.ses-regional-identity
   }
+  smtp_user_infos = (null != var.smtp_user_name) ? {name = module.ses-smtp-user.name, arn = module.ses-smtp-user.arn, password = module.ses-smtp-user.password, login = module.ses-smtp-user.login} : null
 }
 
 module "ses-global" {
--- layers/com-basic/outputs.tf
+++ layers/com-basic/outputs.tf
@@ -2,14 +2,7 @@ output "identities" {
   value = local.identities
 }
 output "smtp_user" {
-  value = (null != var.smtp_user_name)
-    ? {
-        name     = module.ses-smtp-user.name,
-        arn      = module.ses-smtp-user.arn,
-        password = module.ses-smtp-user.password,
-        login    = module.ses-smtp-user.login
-      }
-    : null
+  value       = local.smtp_user_infos
   sensitive   = true
   description = "Optional SES SMTP user credentials"
 }
