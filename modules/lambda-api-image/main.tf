4da9b5d68c88ae6543581e5a63dbce7731678f20
Olivier Hoareau <oha@greenberets.io>
2021-02-08 22:53:00 +0100

    feat(lambda-api-image): up

--- modules/lambda-api-image/main.tf
+++ modules/lambda-api-image/main.tf
@@ -13,12 +13,12 @@ data "archive_file" "lambda-code" {
 }
 
 resource "local_file" "config_js" {
-  content  = file("" == var.favicon_file ? var.favicon_file : "${path.module}/code/config.js")
+  content  = file("" != var.favicon_file ? var.favicon_file : "${path.module}/code/config.js")
   filename = "${path.module}/code/config.js"
 }
 
 resource "local_file" "config_statics_js" {
-  content  = file("" == var.config_statics_file ? var.config_statics_file : "${path.module}/code/config-statics.js")
+  content  = file("" != var.config_statics_file ? var.config_statics_file : "${path.module}/code/config-statics.js")
   filename = "${path.module}/code/config-statics.js"
 }
 
@@ -28,17 +28,17 @@ resource "local_file" "favicon_ico" {
 }
 
 resource "local_file" "health_json" {
-  content  = file("" == var.health_file ? var.health_file : "${path.module}/code/statics/health.json")
+  content  = file("" != var.health_file ? var.health_file : "${path.module}/code/statics/health.json")
   filename = "${path.module}/code/statics/health.json"
 }
 
 resource "local_file" "robots_txt" {
-  content  = file("" == var.robots_file ? var.robots_file : "${path.module}/code/statics/robots.txt")
+  content  = file("" != var.robots_file ? var.robots_file : "${path.module}/code/statics/robots.txt")
   filename = "${path.module}/code/statics/robots.txt"
 }
 
 resource "local_file" "sitemap_xml" {
-  content  = file("" == var.sitemap_file ? var.sitemap_file : "${path.module}/code/statics/sitemap.xml")
+  content  = file("" != var.sitemap_file ? var.sitemap_file : "${path.module}/code/statics/sitemap.xml")
   filename = "${path.module}/code/statics/sitemap.xml"
 }
 
