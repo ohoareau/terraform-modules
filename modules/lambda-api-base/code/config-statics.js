5a066b4ea6aa84702a6a5a9cc70300edabfa6512
Olivier Hoareau <oha@greenberets.io>
2021-02-17 09:14:54 +0100

    feat(lambda-api-*): up

--- modules/lambda-api-base/code/config-statics.js
+++ modules/lambda-api-base/code/config-statics.js
@@ -4,6 +4,26 @@ module.exports = () => ({
         contentType: 'image/x-icon',
         headers: {'Cache-Control': 'public, max-age=60, s-max-age=60'}
     },
+    '/favicon-16x16.png': {
+        name: 'favicon-16x16.png',
+        contentType: 'image/png',
+        headers: {'Cache-Control': 'public, max-age=60, s-max-age=60'}
+    },
+    '/favicon-32x32.png': {
+        name: 'favicon-32x32.png',
+        contentType: 'image/png',
+        headers: {'Cache-Control': 'public, max-age=60, s-max-age=60'}
+    },
+    '/favicon.png': {
+        name: 'favicon-32x32.png',
+        contentType: 'image/png',
+        headers: {'Cache-Control': 'public, max-age=60, s-max-age=60'}
+    },
+    '/apple-touch-icon.png': {
+        name: 'apple-touch-icon.png',
+        contentType: 'image/png',
+        headers: {'Cache-Control': 'public, max-age=60, s-max-age=60'}
+    },
     '/android-chrome-192x192.png': {
         name: 'android-chrome-192x192.png',
         contentType: 'image/png',
@@ -15,6 +35,11 @@ module.exports = () => ({
         headers: {'Cache-Control': 'public, max-age=60, s-max-age=60'}
     },
     '/site.webmanifest': {
+        name: 'site.webmanifest',
+        contentType: 'application/manifest+json',
+        headers: {'Cache-Control': 'public, max-age=60, s-max-age=60'}
+    },
+    '/manifest.json': {
         name: 'site.webmanifest',
         contentType: 'application/manifest+json',
         headers: {'Cache-Control': 'public, max-age=60, s-max-age=60'}
